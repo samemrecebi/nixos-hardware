f6f3cc9aaf466be8e3ac02ecd136976075550c6d
Emre Cebi <emre@cebi.io>
2024-10-31 20:07:18 +0100

    Simpliftyed file first version more testing needed

--- asus/fa507nv/default.nix
+++ asus/fa507nv/default.nix
@@ -10,17 +10,11 @@
     ../../common/cpu/amd/pstate.nix
     ../../common/gpu/nvidia
     ../../common/gpu/nvidia/prime.nix
-    ../../common/hidpi.nix
     ../../common/pc/laptop
     ../../common/pc/ssd
-    ../battery.nix
   ];
 
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.6") pkgs.linuxPackages_latest;
-
-  # The bottom 2 parts are taken from the framework 16-inch laptops configurations.
-  # Workaround for SuspendThenHibernate: https://lore.kernel.org/linux-kernel/20231106162310.85711-1-mario.limonciello@amd.com/
-  boot.kernelParams = lib.optionals (lib.versionOlder config.boot.kernelPackages.kernel.version "6.8") ["rtc_cmos.use_acpi_alarm=1"];
 
   # AMD has better battery life with PPD over TLP:
   # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
@@ -37,7 +31,7 @@
 
   hardware.nvidia = {
     modesetting.enable = lib.mkDefault true;
-    open = lib.mkDefault false;
+    open = lib.mkDefault true;
     nvidiaSettings = lib.mkDefault true;
     prime = {
       amdgpuBusId = "PCI:54:0:0";

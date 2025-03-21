ed271b44a556c4da23060bdbed35e6f9fae09dfa
Emre Çebi <emre@cebi.io>
2025-03-21 16:31:54 +0100

    Update default.nix huawei

--- huawei/machc-wa/default.nix
+++ huawei/machc-wa/default.nix
@@ -1,19 +1,28 @@
 {lib, ...}: {
   imports = [
     ../../common/cpu/intel/comet-lake
+    ../../common/gpu/intel
+    ../../common/gpu/intel/comet-lake
     ../../common/gpu/nvidia
     ../../common/gpu/nvidia/pascal
     ../../common/gpu/nvidia/prime.nix
     ../../common/hidpi.nix
     ../../common/pc/laptop
-    ../../common/pc/ssd
+    ../../common/pc/laptop/ssd
   ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     nvidiaSettings = lib.mkDefault true;
+    modesetting.enable = lib.mkDefault true;
+    powerManagement.enable = lib.mkDefault false;
+    dynamicBoost.enable = lib.mkDefault false;
     prime = {
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  services.auto-cpufreq.enable = true;
 }

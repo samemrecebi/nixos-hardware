6297cb43c689539ec02c0a1bdfd4dca38ed7ba52
Emre Çebi <emre@cebi.io>
2025-03-21 16:54:58 +0100

    Update default.nix Asus

--- asus/fa507nv/default.nix
+++ asus/fa507nv/default.nix
@@ -6,22 +6,38 @@
 }: {
   imports = [
     ../../common/cpu/amd
-    ../../common/cpu/amd/raphael/igpu.nix
+    ../../common/cpu/amd/zenpower.nix
     ../../common/cpu/amd/pstate.nix
+    ../../common/gpu/amd
     ../../common/gpu/nvidia
     ../../common/gpu/nvidia/prime.nix
-    ../../common/gpu/nvidia/turing
-    ../../common/gpu/24.05-compat.nix
+    ../../common/gpu/nvidia/ada-lovelace
     ../../common/pc/laptop
-    ../../common/pc/ssd
+    ../../common/pc/laptop/ssd
     ../../common/hidpi.nix
   ];
 
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.6") pkgs.linuxPackages_latest;
 
-  # AMD has better battery life with PPD over TLP:
-  # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
-  services.power-profiles-daemon.enable = lib.mkDefault true;
+  boot.kernelModules = [ "amdgpu" ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.amdgpu.initrd.enable = false;
+
+  hardware.nvidia = {
+    nvidiaSettings = lib.mkDefault true;
+    modesetting.enable = lib.mkDefault true;
+    powerManagement.enable = lib.mkDefault true;
+    dynamicBoost.enable = lib.mkDefault true;
+    prime = {
+      amdgpuBusId = "PCI:54:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  # Seen better performance then PPD
+  services.auto-cpufreq.enable = true;
 
   # Adds the missing asus functionality to Linux.
   # https://asus-linux.org/manual/asusctl-manual/
@@ -29,14 +45,6 @@
     asusd = {
       enable = lib.mkDefault true;
       enableUserService = lib.mkDefault true;
-    };
-  };
-
-  hardware.nvidia = {
-    nvidiaSettings = lib.mkDefault true;
-    prime = {
-      amdgpuBusId = "PCI:54:0:0";
-      nvidiaBusId = "PCI:1:0:0";
     };
   };
 }

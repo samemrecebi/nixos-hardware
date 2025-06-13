f8623ed876722a700ffd0561e4ea5a6d3c9a297b
Emre Cebi <emre@cebi.io>
2025-06-14 00:09:29 +0200

    chore: uga

--- asus/fa507nv/default.nix
+++ asus/fa507nv/default.nix
@@ -18,13 +18,16 @@
 
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.6") pkgs.linuxPackages_latest;
 
-  # The bottom 2 parts are taken from the framework 16-inch laptops configurations.
-  # Workaround for SuspendThenHibernate: https://lore.kernel.org/linux-kernel/20231106162310.85711-1-mario.limonciello@amd.com/
-  boot.kernelParams = lib.optionals (lib.versionOlder config.boot.kernelPackages.kernel.version "6.8") ["rtc_cmos.use_acpi_alarm=1"];
-
-  # AMD has better battery life with PPD over TLP:
-  # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
-  services.power-profiles-daemon.enable = lib.mkDefault true;
+  boot = {
+    kernelModules = ["kvm-amd"];
+    kernelParams = [
+      "mem_sleep_default=deep"
+      "pcie_aspm.policy=powersupersave"
+      "amdgpu.gpu_recovery=1"
+      "amdgpu.sg_display=0"
+      "amdgpu.dcdebugmask=0x10"
+    ];
+  };
 
   # Adds the missing asus functionality to Linux.
   # https://asus-linux.org/manual/asusctl-manual/
@@ -35,11 +38,20 @@
     };
   };
 
+  boot = {
+    blacklistedKernelModules = ["nouveau"];
+  };
+
+  hardware.amdgpu.initrd.enable = lib.mkDefault true;
+
   hardware.nvidia = {
     modesetting.enable = lib.mkDefault true;
-    open = lib.mkDefault false;
     nvidiaSettings = lib.mkDefault true;
     prime = {
+      offload = {
+        enable = lib.mkDefault true;
+        enableOffloadCmd = lib.mkDefault true;
+      };
       amdgpuBusId = "PCI:54:0:0";
       nvidiaBusId = "PCI:1:0:0";
     };
